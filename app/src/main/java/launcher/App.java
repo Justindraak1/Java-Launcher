d597ee5f08fa69f6a90b2979a26f15b9c7c7677a
Justindraak1 <justinmonne@hotmail.com>
2024-03-01 09:26:12 +0100

    Added Variable List for snake game

--- app/src/main/java/launcher/App.java
+++ app/src/main/java/launcher/App.java
@@ -1,12 +1,31 @@
 package launcher;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 import javax.swing.*;
 
 public class App {
-
+ //test to see if it runs
     public String getGreeting() {
         return "Hello World!";
     }
+ //
+                                    
+ // Variables
+    public ArrayList<Vector> snake = new ArrayList<Vector>();
+        	public Vector pos;
+        public Vector dir = new Vector<>(0, 0);
+        public Integer spd = 20;
+        public Integer len = 5;
+
+    public ArrayList<Vector> snake2 = new ArrayList<Vector>();
+        public Vector pos2;
+        public Vector dir2 = new Vector<>(0, 0);
+        public Integer spd2 = 20;
+        public Integer len2 = 5;
+ //
+
 
     public void start() {
         JFrame frame = new JFrame("Swing Window");
